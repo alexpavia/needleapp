f0d34e2d80227c513f0c08f850852f0f2dd9577a
Alex Pavia <alpavia@gmail.com>
2015-01-29 15:44:09 -0700

    table layout

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = angular.module("table", []);
+
+function TableCtrl($scope){
+	$scope.columnTotals = 4;
+}
+
+
+//Column 1 - Values multiplied by 1.
+var input1 = document.getElementById("column1");
+
+//Column 2 - Values multiplied by 2.
+var input2 = document.getElementById("column2");
+
+//Column 3 - Values multiplied by 3.
+var input3 = document.getElementById("column3");
+
+//Column 4 - Values multiplied by 4.
+var input4 = document.getElementById("column4");
+
+//Column 5 - Values multiplied by 5.
+var input5 = document.getElementById("column5");
+
+//Column 6 - Values multiplied by 6.
+var input6 = document.getElementById("column6");
+
+//Column 7 - Totals
+function sumOfRows (){
+	
+}
+
